8d0ebbdafb4611d8d78477967cbce935f439c514
ayanafikadu9-code <ayanafikadu9@gmail.com>
2026-08-09 19:39:49 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+# ============ CONFIGURATION FILE ============
+BOT_TOKEN = "8913902406:AAE5YB6XyXY4JBXbODODwOTl4P-dnV7T2rA"
+API_URL = "https://silent-mud-7026.codeofsaladin.workers.dev/tiktok"
+WEB_APP_URL = "https://ayanafikadu9-code.github.io/TikTok-Downloader/"
