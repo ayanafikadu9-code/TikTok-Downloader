e34423cdbd8ddd2a947efc482bd302f7d21395f0
ayanafikadu9-code <ayanafikadu9@gmail.com>
2026-08-10 08:19:29 +0300

    database.py

---
+++ database.py
@@ -0,0 +1,9 @@
+user_data = {}
+
+def get_user_attr(user_id, key, default=None):
+    return user_data.get(user_id, {}).get(key, default)
+
+def set_user_attr(user_id, key, value):
+    if user_id not in user_data:
+        user_data[user_id] = {}
+    user_data[user_id][key] = value
