e8666c5dc3029945780c136827753f200a535bce
ayanafikadu9-code <ayanafikadu9@gmail.com>
2026-08-10 08:21:20 +0300

    Update config.py
    Updated bot token and API URLs for the configuration.

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-# ============ CONFIGURATION FILE ============
-BOT_TOKEN = "8913902406:AAE5YB6XyXY4JBXbODODwOTl4P-dnV7T2rA"
-API_URL = "https://silent-mud-7026.codeofsaladin.workers.dev/tiktok"
-WEB_APP_URL = "https://ayanafikadu9-code.github.io/TikTok-Downloader/"
+BOT_TOKEN = "7756185675:AAFFB918jGk8XW60oA1n9V7N8W42p0v614w"
+API_URL = "https://tikwm.com/api/"
+WEB_APP_URL = "https://yourwebsite.com"
